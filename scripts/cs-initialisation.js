05862b4456426935d8995f43208a9ec667cd3510
Tessa Mylonas <tmylonas@creditsense.com.au>
2023-01-18 15:43:28 +1000

    Capturing error codes

--- scripts/cs-initialisation.js
+++ scripts/cs-initialisation.js
@@ -1,24 +1,43 @@
-function logMsg(message) {
-    if (typeof console == "object") console.log(message);
-    else alert(message);
+function logMsg(status, message) {
+    alert = document.getElementById('alert-message');
+    alert.innerHTML = message;
+
+    alert.classList = "";
+    if (status=="success"){
+        alert.classList = "success";
+    } else {
+        alert.classList = "failure";
+    }
+
+    alert.classList.add('activated');
+    setTimeout(function() {
+        alert.classList.remove('activated');
+    }, 5000);
+    setTimeout(function() {
+        alert.classList = "";
+        alert.innerHTML = "Everything is all gucci";
+    }, 10000);
 }
+
 $(document).ready(function() {
     $.CreditSense.Iframe({
         client: "BEEL002",
         elementSelector: "#creditSenseIFrame",
         enableDynamicHeight: true,
         params: {
-            appRef: '<your unique app reference>', // Set this value to a unique reference for the application
-            uniqueAppRef: true, // indicates that the appRef provided is unique
-            cssOverride: "tessamarylaura.github.io/cs-greenfinances/styles/flow-override.css"
+            cssOverride: "tessamarylaura.github.io/cs-greenfinances/styles/flow-override.css",
+            debugBanks: true
         },
         callback: function(response, data) {
             switch (response) {
                 case "99": // Example status code (Bank status success)
-                    logMsg('Bank details collected successfully');
+                    logMsg('success', 'Bank details collected successfully');
                     break;
                 case "100": // Example status code
-                    onApplicationSuccess();
+                    logMsg('success', 'We were able to successfully log in, so this part of your application is complete!');
+                    break;
+                case "-99":
+                    logMsg('failure', "We can't contact the Credit Sense server");
                     break;
             }
         }
